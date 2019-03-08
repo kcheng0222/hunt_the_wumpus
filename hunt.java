30daab42fe4c19311b212eb88a0ac67b957c378e
jn8302 <36803698+jn8302@users.noreply.github.com>
2019-03-08 09:53:41 -0800

    Update hunt.java

--- hunt.java
+++ hunt.java
@@ -32,18 +32,40 @@ public class WumpusGame{
         }
         return false;
     } 
+    public static int[] wrap(int x, int y, String[][] gameMap){
+        
+        
+        if(x == gameMap.length){
+            x = 0;
+        }
+        if(x == -1){
+            x = gameMap.length-1;
+        }
+        if(y == gameMap[0].length){
+            y = 0;
+        }
+        if(y == -1){
+            y = gameMap[0].length-1;
+        }
+        
+        int[] xyCoor = new int[]{x,y};
+        return xyCoor;
+    }
     public static void main(String [] args){
         
         Scanner sc = new Scanner(System.in);
         Random gen = new Random();
         String gameMap[][] = new String[15][15];      
-        
+        int[] tempArr = new int[2];
         gameMap = fillMap(gameMap);
         int playerX = 0;
         int playerY = 0;
         
         int wumpusX = gen.nextInt(gameMap.length-1)+1;
         int wumpusY = gen.nextInt(gameMap[0].length-1)+1;
+        
+        wumpusX = 0;
+        wumpusY = 1;
         
         int pitX = gen.nextInt(gameMap.length-1)+1;
         int pitY = gen.nextInt(gameMap[0].length-1)+1;
@@ -128,6 +150,12 @@ public class WumpusGame{
                     }
                 }
                 System.out.println("You missed. Very bad.");
+                
+                tempArr = wrap(wumpusX,wumpusY,gameMap);
+                
+                wumpusX = tempArr[0];
+                wumpusY = tempArr[1];
+                
                 gameMap[wumpusX][wumpusY] = "#";
             }
             else{
@@ -145,19 +173,13 @@ public class WumpusGame{
                 if(in.equals("d")){
                     playerY += 1;
                 }
+                // player wrap
                 
-                if(playerX == gameMap.length){
-                    playerX = 0;
-                }
-                if(playerX == -1){
-                    playerX = gameMap.length-1;
-                }
-                if(playerY == gameMap[0].length){
-                    playerY = 0;
-                }
-                if(playerY == -1){
-                    playerY = gameMap[0].length-1;
-                }
+                tempArr = wrap(playerX,playerY,gameMap);
+                
+                playerX = tempArr[0];
+                playerY = tempArr[1];
+                
             }
                         
             gameMap[playerX][playerY] = "@";
@@ -187,42 +209,15 @@ public class WumpusGame{
             }else{
                 //tells player if they are next to a pit(breeze), wumpus(sniff), or bat(flap)
                 printMap(gameMap);
-                if((playerX + 1 == wumpusX) && (playerY == wumpusY)){
-                    System.out.println("SNIFF");
-                }
-                else if((playerX - 1 == wumpusX) && (playerY == wumpusY)){
-                    System.out.println("SNIFF");
-                }
-                else if((playerY + 1 == wumpusY) && (playerX == wumpusX)){
-                    System.out.println("SNIFF");
-                }
-                else if((playerY - 1 == wumpusY) && (playerX == wumpusX)){
+                if(nextTo(playerX, playerY, wumpusX, wumpusY)){
                     System.out.println("SNIFF");
                 }
                 
-                if((playerX + 1 == pitX) && (playerY == pitY)){
-                    System.out.println("BREEZE");
-                }
-                else if((playerX - 1 == pitX) && (playerY == pitY)){
-                    System.out.println("BREEZE");
-                }
-                else if((playerY + 1 == pitY) && (playerX == pitX)){
-                    System.out.println("BREEZE");
-                }
-                else if((playerY - 1 == pitY) && (playerX == pitX)){
+                if(nextTo(playerX, playerY, pitX, pitY)){
                     System.out.println("BREEZE");
                 }
                 
-                if((playerX + 1 == batX) && (playerY == batY)){
-                    System.out.println("FLAP");
-                }
-                else if((playerX - 1 == batX) && (playerY == batY)){
-                    System.out.println("FLAP");
-                }
-                else if((playerY + 1 == batY) && (playerX == batX)){
-                    System.out.println("FLAP");
-                }
-                else if((playerY - 1 == batY) && (playerX == batX)){
+                if(nextTo(playerX, playerY, batX, batY)){
                     System.out.println("FLAP");
                 }
                 
